e07692fd92acea51f3bee3ffbaa736ca37ce0faf
Daniel Martinez <56138779+danysora@users.noreply.github.com>
2021-11-23 16:40:26 -0600

    reworked

---
+++ precache-manifest.8e2d8be5de41fcc50a96bdc062a95065.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fdd5944b967c3aaef90bbdb1d63e8cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42127a25f0c7a187ed3d",
+    "url": "/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42127a25f0c7a187ed3d",
+    "url": "/static/js/main.0d5fc4f5.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
